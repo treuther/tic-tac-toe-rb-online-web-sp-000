3d1cbb777463cac2fa7923e0752d9e151f023c9c
Tracy Reuther <tracyreuther@gmail.com>
2019-05-16 23:16:28 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,9 +21,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(array, index, value = "X")
+def move(array, index, value)
 array[index] = value
-
 end
 
 def position_taken?(board, index)
